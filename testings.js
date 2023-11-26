5d1d493d63e8e91591844760724b899583e45643
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-26 09:12:59 -0600

    Update testings.js

--- testings.js
+++ testings.js
@@ -12,23 +12,6 @@ describe('Key Utilities', () => {
   });
 });
 
-//test/keyGenerator.test.js
-const { expect } = require('chai');
-const { generateAndStoreKey } = require('../keyGenerator');
-const db = require('../db');
-
-describe('Key Generator', () => {
-  it('should generate and store RSA private keys in the database', () => {
-    generateAndStoreKey(3600); // Generate a key that expires in 1 hour
-
-    // Query the database for the generated key
-    db.get('SELECT * FROM keys WHERE exp >= ?', [Math.floor(Date.now() / 1000)], (err, row) => {
-      expect(err).to.be.null;
-      expect(row).to.exist;
-      expect(row.exp).to.be.above(Math.floor(Date.now() / 1000));
-    });
-  });
-});
 
 //test/authEndpoint.test.js
 const { expect } = require('chai');
