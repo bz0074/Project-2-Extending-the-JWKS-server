5a4bf9a881ea967c19d2a167d6d3610d32eeac6b
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-26 09:14:39 -0600

    Update testings.js

--- testings.js
+++ testings.js
@@ -11,35 +11,3 @@ describe('Key Utilities', () => {
     expect(deserializedKey).to.equal(privateKey);
   });
 });
-
-
-//test/authEndpoint.test.js
-const { expect } = require('chai');
-const request = require('supertest');
-const app = require('../authEndpoint');
-
-describe('Auth Endpoint', () => {
-  it('should return a JWT for a valid request', (done) => {
-    request(app)
-      .post('/auth')
-      .expect(200)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res.body).to.have.property('token');
-        done();
-      });
-  });
-
-  it('should handle expired query parameter', (done) => {
-    request(app)
-      .post('/auth?expired=true')
-      .expect(200)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res.body).to.have.property('token');
-        done();
-      });
-  });
-
-  // Add more test cases as needed
-});
