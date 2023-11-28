b8468766e214b75a99b32b68d4e855d0d669a7c6
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 17:21:03 -0600

    Update jwtUtils.test.js

--- test/jwtUtils.test.js
+++ test/jwtUtils.test.js
@@ -1,29 +1,10 @@
-// Import necessary modules and functions
-const jwtUtils = require('../src/jwtUtils');
-const db = require('../src/db');
-const response = await request(app).get('/your-endpoint?expired=true');
+const request = require('supertest');
+const app = require('../src/server');
 
-
-// Mock the db.get function
-jest.mock('../src/db', () => ({
-  get: jest.fn(),
-}));
-
-// Test suite for jwtUtils
 describe('jwtUtils Tests', () => {
-  // Test case for the authenticate function
   it('should authenticate successfully', async () => {
-    // Mock the behavior of the db.get function
-    db.get.mockReturnValue({ /* mock data */ });
-
-    // Your authentication test code here
-    const req = { /* mock request object */ };
-    const res = { /* mock response object */ };
-    await jwtUtils.authenticate(req, res);
-
-    // Your assertions here
-    expect(/* your expectations */).toBe(/* expected value */);
+    // Your test logic here
+    const response = await request(app).get('/your-endpoint?expired=true');
+    // Assertions and other test logic
   });
-
-  // Add more test cases as needed for other functions in jwtUtils
 });
