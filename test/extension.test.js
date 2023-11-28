013ca575ddb94ed92dabd7ccb1a4c054fdd8f9c5
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 16:35:11 -0600

    Create extension.test.js

---
+++ test/extension.test.js
@@ -0,0 +1,19 @@
+// Import the module you want to test
+const yourModule = require('../path/to/your/module');
+
+// Describe your test suite
+describe('Your Module Tests', () => {
+  // Test case 1
+  test('Test Case 1', () => {
+    // Your test logic here
+    expect(true).toBe(true);
+  });
+
+  // Test case 2
+  test('Test Case 2', () => {
+    // Your test logic here
+    expect(1 + 1).toBe(2);
+  });
+
+  // ... Add more test cases as needed
+});
