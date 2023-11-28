e7f2fe5a3dd16ab8ac7a3013fb80455a7bd313fc
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 17:30:31 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,3 +1,6 @@
+const { yourTestFunction } = require('./yourTestFile'); // Import any necessary test functions
+
+// Example test without suite
 test("Utility tests: Reloads only when needed", async () => {
   // Your test logic here
 });
