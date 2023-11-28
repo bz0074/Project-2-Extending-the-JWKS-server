38807c4216a9104ea4ca077c062ccbaeae8202e5
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 17:15:09 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,26 +1,5 @@
-// Import the module you want to test
-// Adjust the path based on your project's structure
-const yourModule = require('../src/server.js'); 
-
 describe("Utility tests", () => {
   test("Reloads only when needed", async () => {
     // Your test logic here
   });
-});
-
-// Describe your test suite
-describe('Your Module Tests', () => {
-  // Test case 1
-  test('Test Case 1', () => {
-    // Your test logic here
-    expect(true).toBe(true);
-  });
-
-  // Test case 2
-  test('Test Case 2', () => {
-    // Your test logic here
-    expect(1 + 1).toBe(2);
-  });
-
-  // ... Add more test cases as needed
 });
