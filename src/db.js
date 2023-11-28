7c32817bb0f2f14831967b10a71efc7e8989d879
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 16:08:14 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,19 +1,16 @@
-// db.js
-
 const sqlite3 = require('sqlite3').verbose();
+
+// Open SQLite database connection
 const db = new sqlite3.Database('totally_not_my_privateKeys.db');
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS keys (
-      kid INTEGER PRIMARY KEY AUTOINCREMENT,
-      key BLOB NOT NULL,
-      exp INTEGER NOT NULL
-    )
-  `);
-});
+// Create keys table if not exists
+db.run(`
+  CREATE TABLE IF NOT EXISTS keys(
+    kid INTEGER PRIMARY KEY AUTOINCREMENT,
+    key BLOB NOT NULL,
+    exp INTEGER NOT NULL
+  )
+`);
 
-db.close();
-
-// You may want to export the database object for reuse in other modules
+// Export the database connection
 module.exports = db;
