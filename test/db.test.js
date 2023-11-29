19195c66b0fcd7272e1eede748593228cd6abbf4
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 23:02:05 -0600

    Update db.test.js

--- test/db.test.js
+++ test/db.test.js
@@ -1,11 +1,21 @@
-const db = require('../src/db');
+const db = require('./db');
+const sqlite3 = require('sqlite3').verbose();
 
-describe('Database', () => {
-  it('should have a keys table', done => {
-    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='keys'", (err, row) => {
+beforeAll(() => {
+  // Create an in-memory database for testing
+  db = new sqlite3.Database(':memory:');
+});
+
+test('Database creation', (done) => {
+  db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS keys(kid INTEGER PRIMARY KEY AUTOINCREMENT, key BLOB NOT NULL, exp INTEGER NOT NULL)');
+
+    db.all('SELECT name FROM sqlite_master WHERE type="table" AND name="keys"', (err, rows) => {
       expect(err).toBeNull();
-      expect(row).toBeDefined();
+      expect(rows.length).toBe(1);
       done();
     });
   });
 });
+
+// Add more tests as needed
