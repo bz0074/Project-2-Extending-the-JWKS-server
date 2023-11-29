86b6ce3e0ffbc350f7b78675cb56e32f89738ee8
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 23:02:58 -0600

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -1,36 +1,35 @@
-const request = require('supertest');
-const app = require('../src/server');
+const supertest = require('supertest');
+const app = require('./server');
 
-let server;
+// Test server endpoints
 
-beforeAll((done) => {
-    server = app.listen(4000, () => {
-        console.log('Server is running on port 4000');
-        done();
-    }, 10000);
+// Example: Test if the server is running
+test('GET /', async () => {
+  const response = await supertest(app).get('/');
+  expect(response.status).toBe(200);
+  expect(response.text).toBe('Server is running');
 });
 
-describe('jwtUtils Tests', () => {
-    it('should authenticate successfully', async () => {
-        // Your test logic here
-        const response = await request(app).get('/your-endpoint?expired=true');
-        expect(response.status).toBe(200);
-    });
+// Add more tests as needed
+
+// Example: Test an endpoint with authentication
+test('GET /protected', async () => {
+  const response = await supertest(app)
+    .get('/protected')
+    .set('Authorization', 'Bearer YOUR_VALID_TOKEN');
+
+  expect(response.status).toBe(200);
+  // Add more assertions based on your endpoint behavior
 });
 
-afterAll((done) => {
-    server.close(done);
+// Example: Test an endpoint with invalid authentication
+test('GET /protected (invalid token)', async () => {
+  const response = await supertest(app)
+    .get('/protected')
+    .set('Authorization', 'Bearer INVALID_TOKEN');
+
+  expect(response.status).toBe(401);
+  // Add more assertions based on your endpoint behavior
 });
 
-describe('jwtUtils Tests', () => {
-  it('should authenticate successfully', (done) => {
-    // Your test logic here
-    request(app)
-      .get('/your-endpoint?expired=true')
-      .expect(200)
-      .end((err, res) => {
-        // Handle the response
-        done();
-      });
-  });
-});
+// Add more tests as needed
