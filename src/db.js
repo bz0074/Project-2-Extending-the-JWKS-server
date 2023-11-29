5e935f9e168d0be67bcdaabf2314a14e4bdf1ce3
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:57:16 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,16 +1,41 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-// Open SQLite database connection
-const db = new sqlite3.Database('totally_not_my_privateKeys.db');
+const dbFile = 'totally_not_my_privateKeys.db';
 
-// Create keys table if not exists
-db.run(`
-  CREATE TABLE IF NOT EXISTS keys(
-    kid INTEGER PRIMARY KEY AUTOINCREMENT,
-    key BLOB NOT NULL,
-    exp INTEGER NOT NULL
-  )
-`);
+const initDatabase = async () => {
+  const db = await open({
+    filename: dbFile,
+    driver: sqlite3.Database,
+  });
 
-// Export the database connection
-module.exports = db;
+  // Create the keys table if it doesn't exist
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS keys (
+      kid INTEGER PRIMARY KEY AUTOINCREMENT,
+      key BLOB NOT NULL,
+      exp INTEGER NOT NULL
+    )
+  `);
+
+  return db;
+};
+
+const getPrivateKey = async (expired = false) => {
+  // Implement logic to fetch the private key from the database based on the "expired" parameter
+  // You may need to serialize/deserialize the key
+  // ...
+
+  // For now, let's assume you have a dummy key
+  return 'dummy_private_key';
+};
+
+const getValidPrivateKeys = async () => {
+  // Implement logic to fetch all valid (non-expired) private keys from the database
+  // ...
+
+  // For now, let's assume you have a list of dummy keys
+  return ['dummy_private_key_1', 'dummy_private_key_2'];
+};
+
+module.exports = { initDatabase, getPrivateKey, getValidPrivateKeys };
