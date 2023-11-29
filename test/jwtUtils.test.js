38975ac3222b15dbcac832c23b58a01afb2ec284
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:53:22 -0600

    Update jwtUtils.test.js

--- test/jwtUtils.test.js
+++ test/jwtUtils.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('../src/server');
 
 describe('jwtUtils Tests', () => {
-  it('should authenticate successfully', async () => {
-    // Your test logic here
-    const response = await request(app).get('/your-endpoint?expired=true');
-    // Assertions and other test logic
-  });
+    it('should authenticate successfully', async () => {
+        // Your test logic here
+        const response = await request(app).get('/your-endpoint?expired=true');
+        expect(response.status).toBe(200);
+    });
 });
+
