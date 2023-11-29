1da89ce832380c184301ed8901ef78577021dbc9
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:45:59 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -2,7 +2,7 @@ const { yourTestFunction } = require('server.js');
 
 
 describe("Utility tests", () => {
-    test("Reloads only when needed", () => {
-        // Test logic
+    test("Reloads only when needed", function () {
+        // Your test logic here
     });
 });
