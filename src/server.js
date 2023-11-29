431dc915c5139088b5ffb8604cbabd9a942b98ee
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:56:50 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,39 @@
 const express = require('express');
-const jwtUtils = require('./jwtUtils'); // Import your jwtUtils module here
+const bodyParser = require('body-parser');
+const db = require('./db'); // Assuming you have a separate file for database interactions
+const jwtUtils = require('./jwtUtils');
+
 const app = express();
+const PORT = 3000;
 
-// Middleware to parse JSON requests
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Define your routes
-app.post('/auth', (req, res) => {
-  // Your authentication logic using jwtUtils.authenticate
-  // Example: jwtUtils.authenticate(req, res);
-  res.status(200).json({ token: 'your_generated_token' });
+// Endpoint to handle authentication
+app.post('/auth', async (req, res) => {
+  try {
+    const expired = req.query.expired === 'true';
+    const privateKey = await db.getPrivateKey(expired);
+    const jwt = jwtUtils.signJWT(privateKey);
+    res.json({ token: jwt });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-// Serve JWKS endpoint
-app.get('/.well-known/jwks.json', (req, res) => {
-  // Your JWKS logic
-  // Example: res.status(200).json({ keys: [/* your JWKS keys */] });
+// Endpoint to retrieve JWKS
+app.get('/.well-known/jwks.json', async (req, res) => {
+  try {
+    const privateKeys = await db.getValidPrivateKeys();
+    const jwks = jwtUtils.generateJWKS(privateKeys);
+    res.json(jwks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server
-
-const server = app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
-module.exports = server;
