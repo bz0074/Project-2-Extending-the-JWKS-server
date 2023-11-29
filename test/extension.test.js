4cec080aa8986ff2842c1a73672c75f78a933b0a
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 23:45:24 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -13,3 +13,11 @@ describe('Extension Tests', () => {
 
   // Add more extension tests as needed
 });
+// jest.config.js
+
+module.exports = {
+  // ... other config options
+  transformIgnorePatterns: [
+    "/node_modules/(?!twxs\\.cmake-0\\.0\\.17)"
+  ],
+};
