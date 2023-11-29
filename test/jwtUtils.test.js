32a94f0b54b5d5938d7986578b4b0cd2d5022df1
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 23:01:49 -0600

    Update jwtUtils.test.js

--- test/jwtUtils.test.js
+++ test/jwtUtils.test.js
@@ -1,11 +1,14 @@
-const request = require('supertest');
-const app = require('../src/server');
 
-describe('jwtUtils Tests', () => {
-    it('should authenticate successfully', async () => {
-        // Your test logic here
-        const response = await request(app).get('/your-endpoint?expired=true');
-        expect(response.status).toBe(200);
-    });
+const jwtUtils = require('./jwtUtils');
+const db = require('./db');
+
+test('Load private keys', async () => {
+  // Mock data in the database
+  db.run('INSERT INTO keys (key, exp) VALUES (?, ?)', 'test-key', Date.now() + 3600000);
+
+  const privateKeys = await jwtUtils.loadPrivateKeys();
+  expect(privateKeys).toHaveLength(1);
 });
+
+// Add more tests as needed
 
