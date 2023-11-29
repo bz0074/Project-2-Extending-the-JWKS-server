0e116f7e8ff122eb15e8508b96cff6a3a80c62a9
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:40:02 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,12 +1,6 @@
 const { yourTestFunction } = require('./yourTestFile');
 
-suite("Utility tests", () => {
-    test("Reloads only when needed", function () {
-        // Test logic
-    });
-});
 
-// With this:
 describe("Utility tests", () => {
     test("Reloads only when needed", () => {
         // Test logic
