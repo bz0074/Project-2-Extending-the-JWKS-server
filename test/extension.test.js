d21214941d3760c9cbf05cec422644503efe491c
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-29 00:11:13 -0600

    Update extension.test.js

--- test/extension.test.js
+++ test/extension.test.js
@@ -5,16 +5,13 @@ describe('Utility Tests', () => {
   it('should reload only when needed', () => {
     // Test logic goes here
   });
-
 describe('Extension Tests', () => {
   it('should run an extension test', () => {
     expect(true).to.equal(true); // Placeholder test
   });
-
   // Add more extension tests as needed
 });
 // jest.config.js
-
 module.exports = {
   // ... other config options
   transformIgnorePatterns: [
