7486dc979f6588271af874cc7a33bd87ec8d9f03
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 22:53:59 -0600

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -2,16 +2,23 @@ const request = require('supertest');
 const app = require('../src/server');
 
 let server;
+
 beforeAll((done) => {
     server = app.listen(4000, () => {
         console.log('Server is running on port 4000');
         done();
-    }, 10000); // Increase the timeout to 10000ms
+    }, 10000);
 });
 
+describe('jwtUtils Tests', () => {
+    it('should authenticate successfully', async () => {
+        // Your test logic here
+        const response = await request(app).get('/your-endpoint?expired=true');
+        expect(response.status).toBe(200);
+    });
+});
 
 afterAll((done) => {
-    // Close the server after all tests
     server.close(done);
 });
 
