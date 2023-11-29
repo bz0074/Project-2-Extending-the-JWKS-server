08ad56a41a2807246368fd426fb059b6948ddd1d
bz0074 <128610052+bz0074@users.noreply.github.com>
2023-11-28 23:50:35 -0600

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -2,19 +2,26 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { app } = require('../src/server'); // Adjust the path accordingly
-const expect = chai.expect;
+const app = require('../src/server'); // Adjust the path accordingly
 
 chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe('Server Tests', () => {
-  it('should start the server', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        done();
+  it('should start the server', async () => {
+    // Use an async function to handle server startup
+    await new Promise((resolve) => {
+      app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+        resolve();
       });
+    });
+
+    // Make your HTTP requests here
+    const res = await chai.request(app).get('/');
+    
+    // Assertions go here
+    expect(res).to.have.status(404);
   });
 
   // Add more server tests as needed
